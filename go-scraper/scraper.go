5a3b2240195b2d0161f51ac8b2cd5c5a1c0716a9
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-09-16 18:29:21 +0200

    extract lazy-loaded images

--- go-scraper/scraper.go
+++ go-scraper/scraper.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 
+	"regexp"
+	"strings"
+
 	"github.com/gocolly/colly"
 )
 
@@ -12,13 +15,33 @@ type item struct {
 	price    string `json: "price"`
 }
 
+func extractImageURL(style string) string {
+	// Use a regular expression to find the URL inside the background-image property
+	re := regexp.MustCompile(`url\((.*?)\)`)
+	match := re.FindStringSubmatch(style)
+	if len(match) > 1 {
+		// Ensure the URL is complete (prefix with https if needed)
+		url := match[1]
+		if strings.HasPrefix(url, "//") {
+			url = "https:" + url
+		}
+		return url
+	}
+	return ""
+}
+
 func main() {
 	c := colly.NewCollector(
 		colly.AllowedDomains("shop.esl.com"),
 	)
 
 	c.OnHTML("div.grid-product__content", func(h *colly.HTMLElement) {
-		fmt.Println(h.ChildText(".grid-product__title.grid-product__title--body"))
+		item := item{
+			name:     h.ChildText(".grid-product__title.grid-product__title--body"),
+			price:    h.ChildText(".grid-product__price"),
+			imageURL: extractImageURL(h.ChildAttr(".grid__image-ratio", "style")),
+		}
+		fmt.Println(item)
 	})
 
 	eslLink := ("https://shop.esl.com/?ref=ratcomp&utm_campaign=gs-2020-04-15&utm_source=google&utm_medium=smart_campaign&gad_source=1&gclid=CjwKCAjw6JS3BhBAEiwAO9waF_q7P91RVVwGHOdHSRwxBS-2LFB7nI2d60P338Sy8o-sYKv59Voh_hoCBIYQAvD_BwE")
