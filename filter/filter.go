7d345c92401649ff437d827b580833d5f823aa8f
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-09-27 14:41:11 +0200

    Improve Race Condition description

--- filter/filter.go
+++ filter/filter.go
@@ -1,4 +1,10 @@
-// A race condition occurs when two or more goroutines try to modify a shared var concurrently without proper synchorination.
+// A race condition occurs in concurrent or parallel computing when the behavior of a software system depends on the relative timing or sequence of events,
+// such as the order in which threads or processes execute.
+// This leads to unpredictable outcomes,
+// as the system may produce different results based on the timing of access to shared resources (like memory or variables).
+// In simpler terms, a race condition happens when two or more threads/processes attempt to modify a shared resource at the same time,
+// and the final outcome depends on the sequence in which these accesses occur.
+
 // In this example we create 2 function that increment that increments a shared var.
 
 package main
