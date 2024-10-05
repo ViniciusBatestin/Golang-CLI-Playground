52cee463a61ccb5e0dd3297b2e55dccf40dbaa01
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-10-05 13:15:21 +0200

    Function that returns a channel

---
+++ google2012/generator/generator.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func boring(msg string) <-chan string { // return receives-only channel of string.
+	c := make(chan string)
+	go func() { // we launch the goroutine from inside the function.
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c // return the channel to the caller
+}
+
+func main() {
+	c := boring("boring!") // function returning a channel
+	for i := 0; i < 5; i++ {
+		fmt.Printf("You say: %q\n", <-c)
+	}
+}
