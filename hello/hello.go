ea694ce53a6a1becbeb28b8c7dfe14fea4bc079d
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-10-05 17:24:54 +0200

    fixing path

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 
+	"github.com/ViniciusBatestin/Golang-CLI-Playground/tree/master/greetings"
+
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println(quote.Go())
+
+	//Get a greeting message and prin it.
+	message := greetings.Hello("Vinnys")
+	fmt.Println(message)
 }
