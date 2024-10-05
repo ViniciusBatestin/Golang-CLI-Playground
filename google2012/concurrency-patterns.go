c4de65f7c0799bdaa00857a0f59aa5c2c5bf87c5
ViniciusBatestin <viniciusbatestin@gmail.com>
2024-10-05 12:51:05 +0200

    Work with channels and avoid other WG

--- google2012/concurrency-patterns.go
+++ google2012/concurrency-patterns.go
@@ -6,16 +6,18 @@ import (
 	"time"
 )
 
-func boring(msg string) {
+func boring(msg string, c chan string) {
 	for i := 0; ; i++ {
-		fmt.Println(msg, i)
+		c <- fmt.Sprintf("%s %d", msg, i) // expression to be sent can be any suitable value
 		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 	}
 }
 
 func main() {
-	go boring("Boring!")
-	fmt.Println("I'm listening.")
-	time.Sleep(2 * time.Second)
-	fmt.Println("You're boring; I'm leaving.")
+	c := make(chan string)
+	go boring("Boring!", c)
+	for i := 0; i < 5; i++ {
+		fmt.Printf("You day %q\n", <-c) // Receive Expression is just a value.
+	}
+	fmt.Println("You're boring; I'm leaving")
 }
